3e57b1c3022ba82393e7ba2ed51869e68e75808a
JOJO <hounkpatijosue2020@gmail.com>
2026-04-11 23:55:25 +0000

    feat: ajout des models de parcels, couriers et de tracking

--- apps/couriers/models.py
+++ apps/couriers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+#Courier Profile model
+class CourierProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="courier_profile"
+    )
+
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+
+    is_available = models.BooleanField(default=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} (Courier)"
--- apps/parcels/models.py
+++ apps/parcels/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.conf import settings
+import uuid
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+#Parcel model
+class Parcel(models.Model):
+    STATUS_CHOICES = [
+        ('pending', _('Pending')),
+        ('assigned', _('Assigned')),
+        ("picked", _("Picked Up")),
+        ("in_transit", _("In transit")),
+        ('delivered', _("Delivered")),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='sent_parcels',
+    )
+
+    recruiter_name = models.CharField(max_length=120)
+    receiver_phone = models.CharField(max_length=20)
+
+    pickup_address = models.CharField(max_length=255)
+    delivery_address = models.CharField(max_length=255)
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='pending'
+    )
+
+    assigned_courier = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='deliveries'
+    )
+
+    tracking_code = models.CharField(max_length=20, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.tracking_code} - {self.status}"
