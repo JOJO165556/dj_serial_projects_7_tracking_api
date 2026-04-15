10b527803b0db4cb1fe74e9b2a3c49e689c1e847
JOJO <hounkpatijosue2020@gmail.com>
2026-04-15 20:09:30 +0000

    style: correction manuelle des sauts de ligne et espaces (flake8)

--- apps/realtime/routing.py
+++ apps/realtime/routing.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
 from apps.realtime.consumers import TrackingConsumer
-from apps.notifications.consumers import NotificationConsumer  
+from apps.notifications.consumers import NotificationConsumer
 
 websocket_urlpatterns = [
     re_path(r"ws/tracking/(?P<parcel_code>\w+)/$", TrackingConsumer.as_asgi()),
--- core/responses/base.py
+++ core/responses/base.py
@@ -1,11 +1,13 @@
 from rest_framework.response import Response
 
+
 def success(data=None, message="success", status=200):
     return Response({
         "status": "success",
         "message": message,
         "data": data
     }, status=status)
+
 
 def error(message="error", status=400, data=None):
     return Response({
